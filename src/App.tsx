d548900726988b9a23ee428cbae747ed7879cade
Leonard Lind <konungenleo@gmail.com>
2024-07-22 15:23:24 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Contact from './pages/contact';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='/nicole-holistics/'>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
